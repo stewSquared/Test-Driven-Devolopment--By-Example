5f7709667b7cb432d0a8da49beb21309cd0f8f9d
Stewart Stewart <stewinsalot@gmail.com>
2014-06-27 15:27:08 -0700

    Didn't refactor immediately, but instead tested further

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -1,6 +1,7 @@
 import org.specs2.mutable._
 
 class MoneySpec extends Specification {
+  // test multiplication
   "The amount of five dollars" should {
 
     val five = new Dollar(5)
@@ -16,6 +17,7 @@ class MoneySpec extends Specification {
     }
   }
 
+  // test equality
   "A five dollar object" should {
 
     val five = new Dollar(5)
@@ -23,5 +25,10 @@ class MoneySpec extends Specification {
     "be equivalent to another five dollar object" in {
       five mustEqual new Dollar(5)
     }
+
+    "not be equivalent to a six dollar object" in {
+      five mustNotEqual new Dollar(6)
+    }
+
   }
 }
