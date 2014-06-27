bfbb97c187f8023041bd22894a927204c00be4ca
Stewart Stewart <stewinsalot@gmail.com>
2014-06-27 15:47:03 -0700

    Used functionality just develped to improve a test

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -1,27 +1,20 @@
 import org.specs2.mutable._
 
 class MoneySpec extends Specification {
-  // test multiplication
-  "The amount of five dollars" should {
-
-    val five = new Dollar(5)
-
-    "equal 10 after it's multiplied by 2" in {
-      val product: Dollar = five times 2
-      product.amount mustEqual 10
-    }
-
-    "equal 10 after it's multiplied by 2" in {
-      val product: Dollar = five times 3
-      product.amount mustEqual 15
-    }
-  }
-
-  // test equality
   "A five dollar object" should {
 
     val five = new Dollar(5)
 
+    // test multiplication
+    "equal 10 after it's multiplied by 2" in {
+      five times 2 mustEqual new Dollar(10)
+    }
+
+    "equal 10 after it's multiplied by 2" in {
+      five times 3 mustEqual new Dollar(15)
+    }
+
+    // test equality
     "be equivalent to another five dollar object" in {
       five mustEqual new Dollar(5)
     }
@@ -29,6 +22,5 @@ class MoneySpec extends Specification {
     "not be equivalent to a six dollar object" in {
       five mustNotEqual new Dollar(6)
     }
-
   }
 }
