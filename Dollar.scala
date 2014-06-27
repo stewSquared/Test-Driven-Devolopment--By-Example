4a6d1f0b2fd34c0ebfb6ba4d5ef25ff858940ef1
Stewart Stewart <stewinsalot@gmail.com>
2014-06-27 15:33:35 -0700

    Refactored to capture the two cases at once

--- Dollar.scala
+++ Dollar.scala
@@ -2,6 +2,9 @@ class Dollar(var amount: Int) {
 
   def times(multiplier: Int) = new Dollar(amount * multiplier)
 
-  override def equals(that: Any) = true
+  override def equals(that: Any) = that match {
+    case dollar: Dollar => amount == dollar.amount
+    case _ => throw new ClassCastException
+  }
 
 }
