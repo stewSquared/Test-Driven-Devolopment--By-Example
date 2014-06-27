ab0ed72dfaccfd5834885781425d6b4876199800
Stewart Stewart <stewinsalot@gmail.com>
2014-06-27 14:40:15 -0700

    Translated a design objection (side effects) into a test case that failed because of the objection

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -2,10 +2,18 @@ import org.specs2.mutable._
 
 class MoneySpec extends Specification {
   "The amount of five dollars" should {
+
+    val five = new Dollar(5)
+
     "equal 10 after it's multiplied by 2" in {
-      val five = new Dollar(5)
       five times 2
       five.amount mustEqual 10
     }
+
+    "equal 10 after it's multiplied by 2" in {
+      five times 3
+      five.amount mustEqual 15
+    }
+
   }
 }
