5a9aed4121cde49cac8163e111512ac0ae9b014f
Stewart Stewart <stewinsalot@gmail.com>
2014-06-28 15:24:36 -0700

    Reconciled two implementations -- equals() -- before eliminating the redundant one

--- Franc.scala
+++ Franc.scala
@@ -2,9 +2,4 @@ class Franc(amount: Int) extends Money(amount){
 
   def times(multiplier: Int) = new Franc(amount * multiplier)
 
-  override def equals(that: Any) = that match {
-    case franc: Franc => amount == franc.amount
-    case _ => throw new ClassCastException
-  }
-
 }
