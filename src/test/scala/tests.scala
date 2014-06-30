deb234793f8bb439fea63a70c91de1a6d4aa756b
Stewart Stewart <stewinsalot@gmail.com>
2014-06-30 12:22:09 -0700

    Eliminated tests that made sense with the old code structure but were redundant with the new code structure

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -3,29 +3,20 @@ import org.specs2.mutable._
 class MoneySpec extends Specification {
   "When multiplied by n, the amount of the yielded money object" should {
 
-    val fiveDollars: Money = Money.dollar(5)
-    val fiveFrancs = Money.franc(5)
+    val fiveDollars = Money.dollar(5)
 
-    "equal [the original money's amount] * n" in {
+    "equal the original money object's amount * n" in {
       fiveDollars times 2 mustEqual Money.dollar(10)
       fiveDollars times 3 mustEqual Money.dollar(15)
-
-      fiveFrancs times 2 mustEqual Money.franc(10)
-      fiveFrancs times 3 mustEqual Money.franc(15)
     }
   }
 
   "A comparison of two money objects" should {
-    "show dollars to be equal iff they have the same amount" in {
+    "show them to be equal iff they have the same amount" in {
       Money.dollar(5) mustEqual Money.dollar(5)
       Money.dollar(5) mustNotEqual Money.dollar(6)
     }
-    "show francs to be equal iff they have the same amount" in {
-      Money.franc(5) mustEqual Money.franc(5)
-      Money.franc(5) mustNotEqual Money.franc(6)
-    }
-    "show two units of differing currency to be unequal"
-    "yield false if they are not the same currency, but share the same amount" in {
+    "show two units of differing currency to be unequal" in {
       Money.franc(1) mustNotEqual Money.dollar(1)
     }
   }
