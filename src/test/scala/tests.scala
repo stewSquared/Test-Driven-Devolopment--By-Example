a0b6c5a985b014edd65bda7d5e2eb36bf5f3d555
Stewart Stewart <stewinsalot@gmail.com>
2014-06-29 21:30:48 -0700

    Decoupled test code from the existence of concrete subclasses by introducing factory methods

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -7,8 +7,8 @@ class MoneySpec extends Specification {
     val fiveFrancs = new Franc(5)
 
     "equal [the original money's amount] * n" in {
-      fiveDollars times 2 mustEqual new Dollar(10)
-      fiveDollars times 3 mustEqual new Dollar(15)
+      fiveDollars times 2 mustEqual Money.dollar(10)
+      fiveDollars times 3 mustEqual Money.dollar(15)
 
       fiveFrancs times 2 mustEqual new Franc(10)
       fiveFrancs times 3 mustEqual new Franc(15)
@@ -17,8 +17,8 @@ class MoneySpec extends Specification {
 
   "A comparison of two money objects" should {
     "show dollars to be equal iff they have the same amount" in {
-      new Dollar(5) mustEqual new Dollar(5)
-      new Dollar(5) mustNotEqual new Dollar(6)
+      Money.dollar(5) mustEqual Money.dollar(5)
+      Money.dollar(5) mustNotEqual Money.dollar(6)
     }
     "show francs to be equal iff they have the same amount" in {
       new Franc(5) mustEqual new Franc(5)
@@ -26,7 +26,7 @@ class MoneySpec extends Specification {
     }
     "show two units of differing currency to be unequal"
     "yield false if they are not the same currency, but share the same amount" in {
-      new Franc(1) mustNotEqual new Dollar(1)
+      new Franc(1) mustNotEqual Money.dollar(1)
     }
   }
 }
