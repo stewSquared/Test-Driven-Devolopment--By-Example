2f1efcc5774174deae6de826354e27242d3fdc4e
Stewart Stewart <stewinsalot@gmail.com>
2014-07-03 20:30:45 -0700

    Introduced a private helper class without distinct tests of its own

--- Bank.scala
+++ Bank.scala
@@ -1,4 +1,18 @@
+import collection.mutable.HashMap
+
 class Bank {
+
+  private val rates = new HashMap[Pair, Int]
+
+  class Pair(val from: String, val to: String) {
+
+    override def equals(that: Any) = that match {
+      case pair: Pair => from.equals(pair.from) && to.equals(pair.to)
+      case _ => throw new ClassCastException
+    }
+
+    override def hashCode() = 0
+  }
 
   def reduce(source: Expression, to: String) = source.reduce(this, to)
 
@@ -6,5 +20,4 @@ class Bank {
     if (from.equals("CHF") && to.equals("USD")) 2 else 1
 
   def addRate(from: String, to: String, rate: Int) = ???
-
 }
